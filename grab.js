9464511e166276aba215814a35c80e34f230fac5
Jacob Elder <jacob@boundless.com>
2013-12-05 17:28:09 -0500

    Simplified now that I know how to use `convert -crop` correctly (don't).

--- grab.js
+++ grab.js
@@ -6,32 +6,24 @@ if (/http:.*\.dev/.test(url1)) {
   url2 = url1.replace(/^https:/, 'http:').replace(/.com/,'.dev');;
 }
 
-// url2 = 'http://www.reddit.com/'
+var page = require('webpage').create();
 
 // PhantomJS defaults to a transparent background
-var reset = function() {
+page.evaluate(function() {
   var style = document.createElement('style'),
       text = document.createTextNode('body { background: #fff }');
   style.setAttribute('type', 'text/css');
   style.appendChild(text);
   document.head.insertBefore(style, document.head.firstChild);
-};
+});
+page.viewportSize = { width: 1366, height: 768 };
 
-var page1 = require('webpage').create();
-page1.evaluate(reset);
-page1.viewportSize = { width: 1366, height: 768 };
-
-var page2 = require('webpage').create();
-page2.evaluate(reset);
-page2.viewportSize = { width: 1366, height: 768 };
-
-page1.open(url1, function () {
+page.open(url1, function () {
   console.log('Saved '+url1);
-  page1.render('url1.png');
-  page2.open(url2, function () {
+  page.render('url1.png');
+  page.open(url2, function () {
     console.log('Saved '+url2);
-    page2.render('url2.png');
+    page.render('url2.png');
     phantom.exit();
   });
 });
-
